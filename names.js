9faa8283e529567f632fa09d892e924b64fdd342
JeromeGH <jerome.e.morrow@gmail.com>
2026-05-30 14:41:51 -0400

    Add files via upload

---
+++ names.js
@@ -0,0 +1,10 @@
+// Este archivo reemplaza a names.txt para que funcione desde file://
+// Edita solo el contenido entre las comillas invertidas (`...`)
+// Mismo formato que names.txt: número TAB nombre, una línea por app
+
+window.NAMES_RAW = `01	CMD
+02	Hal 9000
+03	Pixaroma
+04	Mi App 04
+05	Otra App 05 mejor
+`;
